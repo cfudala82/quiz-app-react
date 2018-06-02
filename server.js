e7d1d00201d3290cd792a7da8604db6006869760
Christopher Fudala <cfudala82@me.com>
2018-06-01 22:52:21 -0500

    timer, changed color scheme

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ io.on('connection', (client) => {
   client.on('subscribeToTimer', (interval) => {
     console.log('client is subscribing to timer with interval ', interval);
     setInterval(() => {
-      client.emit('timer', {new Date()});
+      client.emit('timer', new Date());
     }, interval);
   });
 });
 
 
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 io.listen(port);
 console.log('listening on port ', port);
